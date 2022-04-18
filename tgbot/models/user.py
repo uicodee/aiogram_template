804d2b3aebece6146e76d1fb4abe8409b951c3bd
uicode <uicodee@gmail.com>
2022-04-18 21:47:24 +0500

    Base and user models

---
+++ tgbot/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, BigInteger, String
+
+from tgbot.models import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    user_id = Column(BigInteger, nullable=False, autoincrement=False, primary_key=True)
+    name = Column(String(length=60), nullable=False)
+    username = Column(String(length=100), nullable=True)
+
+    def __repr__(self):
+        return f'{self.user_id} | {self.name} | {self.username}'
