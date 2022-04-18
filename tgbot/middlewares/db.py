8986dc714d92e4dbe72aff08879fca467b030cd8
uicode <uicodee@gmail.com>
2022-04-18 21:45:34 +0500

    DB middleware to get session object in handler

--- tgbot/middlewares/db.py
+++ tgbot/middlewares/db.py
@@ -1,22 +1,18 @@
 from aiogram.dispatcher.middlewares import LifetimeControllerMiddleware
 
-from tgbot.services.repository import Repo
 
-
-class DbMiddleware(LifetimeControllerMiddleware):
+class DbSessionMiddleware(LifetimeControllerMiddleware):
     skip_patterns = ["error", "update"]
 
-    def __init__(self, pool):
+    def __init__(self, session_pool):
         super().__init__()
-        self.pool = pool
+        self.session_pool = session_pool
 
     async def pre_process(self, obj, data, *args):
-        db = await self.pool.acquire()
-        data["db"] = db
-        data["repo"] = Repo(db)
+        session = self.session_pool()
+        data["session"] = session
 
     async def post_process(self, obj, data, *args):
-        del data["repo"]
-        db = data.get("db")
-        if db:
-            await db.close()
+        session = data.get("session")
+        if session:
+            await session.close()
