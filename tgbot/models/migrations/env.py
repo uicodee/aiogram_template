b5122a06bb7ce98d616f9acf1020b7ee240a516f
uicodee <uicodee@gmail.com>
2022-05-02 14:42:30 +0500

    Fixed alembic .env file

--- tgbot/models/migrations/env.py
+++ tgbot/models/migrations/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from tgbot.models.base import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
