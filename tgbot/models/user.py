9bb899f3530b17557e4e1f8279e4546881c9eead
uicodee <uicodee@gmail.com>
2022-07-08 01:11:31 +0500

    added field language for language code

--- tgbot/models/user.py
+++ tgbot/models/user.py
@@ -9,6 +9,7 @@ class User(BaseModel):
     user_id = Column(BigInteger, nullable=False, autoincrement=False, primary_key=True)
     name = Column(String(length=60), nullable=False)
     username = Column(String(length=100), nullable=True)
+    language = Column(String(length=10), nullable=False)
 
     def __repr__(self):
-        return f'{self.user_id} | {self.name} | {self.username}'
+        return f'{self.user_id} | {self.name} | {self.username} | {self.language}'
