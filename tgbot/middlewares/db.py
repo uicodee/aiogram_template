59c8526bf354f22bd25e5e185a73fd650cc481f6
uicodee <uicodee@gmail.com>
2022-07-08 01:10:56 +0500

    added repo to middleware

--- tgbot/middlewares/db.py
+++ tgbot/middlewares/db.py
@@ -1,18 +1,25 @@
 from aiogram.dispatcher.middlewares import LifetimeControllerMiddleware
+from sqlalchemy.orm import sessionmaker
+
+from tgbot.service.repo.repository import SQLAlchemyRepos
 
 
 class DbSessionMiddleware(LifetimeControllerMiddleware):
     skip_patterns = ["error", "update"]
 
-    def __init__(self, session_pool):
+    def __init__(self, session_pool: sessionmaker):
         super().__init__()
         self.session_pool = session_pool
 
     async def pre_process(self, obj, data, *args):
         session = self.session_pool()
-        data["session"] = session
+        data['session'] = session
+        repo = SQLAlchemyRepos(session)
+        data['repo'] = repo
 
     async def post_process(self, obj, data, *args):
         session = data.get("session")
         if session:
             await session.close()
+            del data["session"]
+            del data["repo"]
