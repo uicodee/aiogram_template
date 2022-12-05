9db281ef4158bffe4bfab326e05d221b8358eb87
uicodee <uicodee@gmail.com>
2022-12-05 18:56:04 +0500

    simplified project structure

--- tgbot/data/data.py
+++
@@ -1,9 +0,0 @@
-from tgbot.middlewares.language import I18nMiddleware
-
-
-i18n = I18nMiddleware(domain="messages", path="tgbot/locales", default="uz")
-
-languages = {
-    "ru": "🇷🇺 Русский",
-    "uz": "🇺🇿 O'zbek"
-}
